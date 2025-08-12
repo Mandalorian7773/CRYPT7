e1446c39d8c031c08c35badf01c77da7a5a5b565
Aditya Jagrani <adityajagrani@Adityas-MacBook-Air.local>
2025-08-12 23:49:19 +0530

    ui improvements

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,20 +6,37 @@ import { lockWallet } from './../utils/walletLocker.ts';
 const Navbar = () => {
   const dispatch = useDispatch();
   const isUnlocked = useSelector((state: RootState) => state.wallet.isUnlocked);
+
   const handleLock = () => {
     dispatch(lockWallet());
   }
 
   return (
-    <nav className="bg-gray-950 text-white px-6 py-4 flex justify-center items-center shadow">
-      <div className="flex gap-10">
+    <nav className="bg-gray-950 text-white px-6 py-4 flex items-center shadow">
+
+      <div className="text-3xl font-extrabold">
+        <span className="text-white">CRYPT</span>
+        <span className="text-sky-400">7</span>
+      </div>
+
+      <div className="flex-1 flex justify-center gap-10">
         <NavLink to="/" className={({ isActive }: { isActive: boolean }) => isActive ? 'text-blue-400 font-semibold' : 'hover:text-blue-300'}>Home</NavLink>
         <NavLink to="/explore" className={({ isActive }: { isActive: boolean }) => isActive ? 'text-blue-400 font-semibold' : 'hover:text-blue-300'}>Explore</NavLink>
         <NavLink to="/wallets" className={({ isActive }: { isActive: boolean }) => isActive ? 'text-blue-400 font-semibold' : 'hover:text-blue-300'}>Wallets</NavLink>
-        {isUnlocked && <button className="bg-black w-20 rounded-xl" onClick={handleLock}>Lock</button>}
+      </div>
+      
+      <div>
+        {isUnlocked && (
+          <button className="bg-black w-20 rounded-xl" onClick={handleLock}>
+            Lock
+          </button>
+        )}
       </div>
     </nav>
   )
 }
 
 export default Navbar;
+
+
+
